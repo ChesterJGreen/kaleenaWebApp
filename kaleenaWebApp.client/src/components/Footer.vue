c4d13f3910168b808dea8c8ee2d54dec7f612f94
CJ Green <seejarom@gmail.com>
2023-04-14 22:42:15 -0600

    got images

---
+++ kaleenaWebApp.client/src/components/Footer.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="container-fluid mt-5">
+      <div class="row pinkSquareBorder">
+        <div class="col-10 offset-1 pinkBorder">
+          <div class="row">
+            <div class="col-3 p-5">
+              <img src="src\assets\img\LogoTransparentEnlargedCropped.png" width="300">
+            </div>
+            <div class="col-6 p-5 text-center">
+              <div class="row">
+                <div class="col-12 pt-5" id="SweetBites">
+                  <p class="ThanksText">Thanks for stopping by!</p>
+                  <button class="btn rounded p-4 keepInTouch" data-toggle="modal"
+                              data-target="#newsletterModal">Let's keep in touch</button>
+                    <img src="src\assets\img\Arrow.png" id="Arrow">
+                    <p id="SBText">Sweet Bites</p>
+                  <p id="NewsLetter">Newsletter</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-3 pt-5">
+              <img src="src\assets\img\StampPic.png">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+      </div>
+      <div class="row">
+      </div>
+      <nav class="navbar navbar-expand-lg px-5">
+        <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+          <div class="d-flex flex-column align-items-center">
+          </div>
+          <div class="">
+            <img src="src\assets\img\Transparent Logo Cropped.png" class="rounded-pill w-50" alt="">
+          </div>
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto text-dark">
+            <li>
+              <router-link :to="{ name: 'About' }" class="btn text-success darken-30 selectable text-uppercase">
+                About Me
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="{ name: 'Books' }" class="btn text-success darken-30 selectable text-uppercase">
+                Books
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="{ name: 'Contact' }" class="btn text-success darken-30 selectable text-uppercase">
+                Contact
+              </router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav justify-content-right justify-content-end">
+            <ul>
+              <a href="https://www.facebook.com/kaleena.shreeve"><img width="25"
+                  src="src\assets\img\f_logo_RGB-Black_144.png"></a>
+            </ul>
+            <ul>
+              <a href="https://www.instagram.com/kaleenashreeve1/"><img width="25"
+                  src="src\assets\img\Instagram_Glyph_Black.png"></a>
+            </ul>
+          </ul>
+        </div>
+  
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style scoped>
+  a:hover {
+    text-decoration: none;
+  }
+  .nav-link {
+    text-transform: uppercase;
+  }
+  .navbar-nav .router-link-exact-active {
+    border-bottom: 2px solid var(--bs-success);
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  li {
+    font-family: cursive;
+  }
+  button {
+    font-size: x-large;
+  }
+  .pinkBorder {
+    border: solid 12px;
+    border-color: rgb(250, 183, 224);
+    border-radius: 12px;
+  }
+  .ThanksText {
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 300%;
+  }
+  #SweetBites {
+    position: relative;
+  }
+  #Arrow {
+    position: absolute;
+    top: 10em;
+    left: 6em;
+    width: 21%;
+    transform: scaleY(-1);
+  }
+  #SBText {
+    font-size: 400%;
+    font-family: 'Rochester', serif;
+  }
+  .keepInTouch {
+    background-color: rgb(201 210 188);
+  }
+  #NewsLetter {
+    font-size: 300%;
+    font-family: 'Volk', serif;
+  }
+  </style>
--- kaleenaWebApp.client/src/components/Navbar.vue
+++ kaleenaWebApp.client/src/components/Navbar.vue
@@ -1,64 +1,135 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+  <div class="container-fluid">
+    <nav class="row navbar navbar-expand-lg">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-3 p-2 offset-9" id="SweetBites">
+            <img src="src\assets\img\Arrow.png" id="Arrow">
+            <span class="text-start" id="SBText">Sweet Bites</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-2 ms-5">
+            <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+              <div class="d-flex flex-column align-items-center">
+                <img src="src\assets\img\LogoTransparentEnlargedCropped.png" class="rounded-pill w-100" alt="">
+              </div>
+            </router-link>
+          </div>
+          <div class="col-9">
+            <div class="collapse navbar-collapse pt-3" id="navbarText">
+              <ul class="navbar-nav justify-content-right justify-content-end">
+                <ul class="navbar-nav me-auto text-dark volk d-flex flex-row">
+                  <div class="row ">
+                    <div class="col-9 d-flex">
+                      <Div class="row">
+                      <li class="col-3">
+                        <router-link :to="{ name: 'About' }" class="router btn text-dark selectable text-uppercase">
+                          About Me
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Books' }" class="router btn text-dark selectable text-uppercase">
+                          Books
+                        </router-link>
+                      </li>
+                      <li class="col-3">
+                        <router-link :to="{ name: 'Journeys' }" class="router btn text-dark selectable text-uppercase">
+                          Journeys
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Blog' }" class="router btn text-dark selectable text-uppercase">
+                          Blog
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Contact' }" class="router btn text-dark selectable text-uppercase">
+                          Contact
+                        </router-link>
+                      </li>
+                    </Div>
+                    </div>
+                    <div class="col-2 d-flex ms-5">
+                      <li>
+                        <a href="https://www.facebook.com/kaleena.shreeve"><img width="35"
+                            src="src\assets\img\f_logo_RGB-Black_144.png"></a>
+                      </li>
+                      <li class="mx-2">
+                        <a href="https://www.instagram.com/kaleenashreeve1/"><img width="35"
+                            src="src\assets\img\Instagram_Glyph_Black.png"></a>
+                      </li>
+                      <li class="me-2">
+                        <a href="https://www.pinterest.com/kaleenashreeve/"><img width="35"
+                            src="src\assets\img\P-Badge-Cosmicore-Black-900.png"></a>
+                      </li>
+                      <li>
+                        <span class="p-1 ">
+                          <button class="btn text-white rounded-pill bg-primary volk p-1.5 ms-3" data-toggle="modal"
+                            data-target="#newsletterModal">Newsletter</button>
+                        </span>
+                      </li>
+                    </div>
+                  </div>
+                </ul>
+              </ul>
+            </div>
+          </div>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+          </button>
+        </div>
       </div>
-    </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
-import Login from './Login.vue'
 export default {
   setup() {
-    return {}
+    return {};
   },
-  components: { Login }
-}
+};
 </script>
 
 <style scoped>
 a:hover {
   text-decoration: none;
 }
-
 .nav-link {
   text-transform: uppercase;
 }
-
 .navbar-nav .router-link-exact-active {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
-
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
+.volk {
+  font-family: 'Vollkorn', serif;
+  font-size: 30px;
 }
-
+.router {
+  font-size: 85%;
+}
+button {
+  font-family: cursive;
+}
+#SweetBites {
+  position: relative;
+}
+#Arrow {
+  position: absolute;
+  top: .5em;
+  left: 8em;
+  width: 15%;
+}
+#SBText {
+  position: absolute;
+  top: -0.25em;
+  left: 5.25em;
+  font-size: xx-large;
+  font-family: 'Rochester', serif;
+}
 </style>
