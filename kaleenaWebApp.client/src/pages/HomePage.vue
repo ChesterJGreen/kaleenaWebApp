cee7256714d92f05a8cffa4bcc168720e016012c
CJ Green <seejarom@gmail.com>
2023-04-17 22:32:33 -0600

    pink buttons on main page

--- kaleenaWebApp.client/src/pages/HomePage.vue
+++ kaleenaWebApp.client/src/pages/HomePage.vue
@@ -33,7 +33,7 @@
             <img src="src\assets\img\CowboyBlack.png" width="150">
           </div>
           <div class="col-9 text-center ifStatements px-4">
-            <button class="btn text-black rounded-pill bg-primary p-1.5 py-2 px-3 rochester" data-toggle="modal"
+            <button class="btn text-black rounded-pill pink-button p-1.5 py-2 px-3 rochester" data-toggle="modal"
                             data-target="">c l i c k &nbsp&nbsp h e r e</button>
             <p class="mt-3">if a <b>hot cowboy</b></p>
             <p>staring at you would make you blush</p>
@@ -45,7 +45,7 @@
       <div class="col-6">
         <div class="row">
           <div class="col-9 text-center ifStatements px-4">
-            <button class="btn text-black rounded-pill bg-primary p-1.5 py-2 px-3 rochester" data-toggle="modal"
+            <button class="btn text-black rounded-pill pink-button p-1.5 py-2 px-3 rochester" data-toggle="modal"
             data-target="">c l i c k &nbsp&nbsp h e r e</button>
             <p class="mt-3">if you believe</p>
             <p><b>love and romance</b> is all around you</p>
