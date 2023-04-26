9053451df1528e862f75614fb51fc8ef667ab6fa
CJ Green <seejarom@gmail.com>
2023-04-25 22:59:46 -0600

    layout for about me done

--- kaleenaWebApp.client/src/pages/AboutPage.vue
+++ kaleenaWebApp.client/src/pages/AboutPage.vue
@@ -22,12 +22,39 @@
             <span>Seeker of Happines</span>
           </div>
         </div>
+        <div class="row">
+          <div class="col-12">
+            <span><a href="http://www.kaleenashreeve.com">GET TO KNOW ME</a></span>
+          </div>
+        </div>
       </div>
       <div class="col-4 mt-5">
         <img src="src\assets\img\KaleenaPortrait.jpg" alt="" width="500">
       </div>
-      <div class="col-4"></div>
+      <div class="col-4 mt-5 text-center">
+        <div class="row">
+          <div class="col-12 mt-5">
+            <span>If you like</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>enchanting stories,</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>you are in the right</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>place.</span>
+          </div>
+        </div>
+      </div>
     </div>
+    <Footer />
   </div>
 </template>
 
