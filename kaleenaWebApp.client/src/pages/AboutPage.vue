65862d4f60e10a31f72b419770f9e5cc36e8aa27
CJ Green <seejarom@gmail.com>
2023-04-25 22:54:29 -0600

    added image for about me

--- kaleenaWebApp.client/src/pages/AboutPage.vue
+++ kaleenaWebApp.client/src/pages/AboutPage.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="about container-fluid my-5 bg-white">
+  <div class="container-fluid bg-white">
     <div class="row">
-      <div class="clo-12 mt-5">
-        <h1>This is the About Me page</h1>
+      <div class="col-4 text-center mt-5">
+        <div class="row">
+          <div class="col-12 mt-5">
+            <span>Hi! I'm Kaleena</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>Author</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>Lover of Romance</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span>Seeker of Happines</span>
+          </div>
+        </div>
       </div>
+      <div class="col-4 mt-5">
+        <img src="src\assets\img\KaleenaPortrait.jpg" alt="" width="500">
+      </div>
+      <div class="col-4"></div>
     </div>
   </div>
 </template>
