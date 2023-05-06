4285ea64b69d32cd455c3ec7fe752b784b662dd7
CJ Green <seejarom@gmail.com>
2023-05-05 22:50:05 -0600

    added blog model properties

---
+++ kaleenaWebApp.client/src/models/Blog.js
@@ -0,0 +1,15 @@
+export class Blog {
+    constructor(data) {
+      this.id = data.id
+      this.name = data.name
+      this.title = data.title
+      this.header = data.header
+      this.body = data.body
+      this.quote = data.quote
+      this.quote2 = data.quote2
+      this.percentage = data.percentage
+      this.picture = data.picture
+      this.link = data.link
+    }
+  }
+  
